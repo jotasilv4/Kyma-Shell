41e0ec1e206b6cebee1f9550e33921b9afb372cd
Jackson Barbosa <jbsilva1337@gmail.com>
2025-02-21 12:12:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ fi
 
 # Instala o gray-git
 echo "Instalando gray-git..."
-yay -Syy --needed --confirm gray-git || true
+yes | yay -Syy --needed --confirm gray-git || true
 
 # Instala os pacotes necessários via yay
 echo "Instalando pacotes necessários com yay..."
